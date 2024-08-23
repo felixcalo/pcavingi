4b295227e288946b76cc0b3fcc8de837c6666e21
felixcalo <felixcalo12@hotmail.com>
2024-08-23 17:31:03 +0100

    feat:pcavingi

---
+++ src/components/navigation.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+export const Navigation = (props) => {
+  return (
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <img src="assets/logo.png" alt='PCAVINGI'className="logo" />
+        </div>
+
+        <div
+          className="collapse navbar-collapse "
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right ">
+            <li>
+              <a href="#features" className="page-scroll">
+                Features
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="page-scroll">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#services" className="page-scroll">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" className="page-scroll">
+                Gallery
+              </a>
+            </li>
+            <li>
+              <a href="#testimonials" className="page-scroll">
+                Testimonials
+              </a>
+            </li>
+            <li>
+              <a href="#team" className="page-scroll">
+                Team
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="page-scroll">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
